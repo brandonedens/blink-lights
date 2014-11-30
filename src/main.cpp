09861aab33df335ea1c73b6182e9d0d45816b23e
Brandon Edens <brandonedens@gmail.com>
2014-11-30 11:25:07 -0800

    Remove textural sample data printing.

--- src/main.cpp
+++ src/main.cpp
@@ -31,17 +31,6 @@ using namespace bl;
 
 /******************************************************************************/
 
-/** Print sample data. */
-static void print_samples(int16_t* data, size_t sz)
-{
-	for (auto i = 0U; i < sz; i += 5) {
-		for (auto j = 0; j < data[i]; j += 30) {
-			cout << "*";
-		}
-		cout << endl;
-	}
-}
-
 int main(int argc, char *argv[])
 {
 	QApplication app{argc, argv};
@@ -50,11 +39,7 @@ int main(int argc, char *argv[])
 
 	OpenAlCapture cap{};
 	thread t{&OpenAlCapture::start, &cap, [&win](int16_t* data, size_t sz) {
-#if 1
 		win.add_data(data, sz);
-#else
-		print_samples(data, sz);
-#endif
 	}};
 
 	return app.exec();
